f6f1973c151bee670005cf321db0ac78075dfd47
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-11-02 01:54:38 +0500

    dsfds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import DataSetter from "./data-setter";
 import ProductsComponent from "@/components/Products/Products";
 
 export default async function Home() {
-  // const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"; // Fallback to localhost
-  const response = await fetch(`/api/products`);
+  const baseUrl =
+    process.env.NEXT_PUBLIC_API_URL || `https://${process.env.VERCEL_URL}/api`;
+
+  const response = await fetch(`${baseUrl}/products`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch data");
