ba6678282d470a72a9d2c83dce7865406c7ec5ff
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-11-02 00:57:34 +0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Product } from "@/lib/types";
 import DataSetter from "./data-setter";
 import ProductsComponent from "@/components/Products/Products";
+
 export default async function Home() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`
-  );
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"; // Fallback to localhost
+  const response = await fetch(`${baseUrl}/api/products`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
   const data: Product[] = await response.json();
 
