db021e57cb5f7e94dc4016f42197ff587e980f6f
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-11-02 00:28:14 +0500

    add env file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import { Product } from "@/lib/types";
 import DataSetter from "./data-setter";
 import ProductsComponent from "@/components/Products/Products";
 export default async function Home() {
-  const response = await fetch("http://localhost:3000/api/products");
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`
+  );
+
   const data: Product[] = await response.json();
 
   return (
